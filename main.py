43081e177186dec53d08946634d850c0b61601be
mkrooted <koreshov.m@gmail.com>
2020-10-25 19:24:35 +0200

    Second (quick) usage scenario. Fixes. More logging.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import logging
 import os
 import time
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, PicklePersistence
 from telegram import ParseMode
 
 # Enable logging
@@ -13,12 +13,14 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # States
-FILENAME, CONTENT = range(2)
+FILENAME, CONTENT, QUICK_FILENAME = range(3)
 
 # pdf converter
 DEFAULT_QUALITY = 100
 MAGICK_BIN = 'convert'
 MAGICK, IMG2PDF = range(2)
+MAX_IMG_N = 100
+MAX_FILENAME_LEN = 60
 
 def pdfcmd(files, pdfname, type=MAGICK, quality=DEFAULT_QUALITY):
     if type == MAGICK:
@@ -29,65 +31,7 @@ def pdfcmd(files, pdfname, type=MAGICK, quality=DEFAULT_QUALITY):
         raise NotImplementedError()
 pdf_converter = IMG2PDF
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
 
-
-def help_command(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-# --------------------------
-def newpdf(update, context):
-    """Start new PDF"""
-    update.message.reply_text("New PDF. Enter document name (`[a-zA-Z0-9_.]`)")
-    return FILENAME
-
-def invalid_filename(update, context):
-    """Prompt one more time"""
-    update.message.reply_text("Invalid name. Please, use only `a-z, A-Z, 0-9, _` and `.` characters. Try again or send /cancel")
-    return FILENAME
-
-def filename_input(update, context):
-    """valid filename entered. proceed to content"""
-    context.user_data['filename'] = update.message.text
-    context.user_data['images'] = 0
-    update.message.reply_text(
-        f'Got it. Now send content of the <code>{update.message.text}.pdf</code> (*.jpg files without tg compression or any tg-compressed images)\n'
-        'When you are ready to compile pdf, send /compile. Send /cancel to cancel.\n'
-        '<i>Note: pdf pages will have the same orientation as original images. Therefore you need to rotate them before sending</i>',
-        parse_mode=ParseMode.HTML
-    )
-    return CONTENT
-
-def addfile(update, context):
-    """input: .jpg file"""
-    uid = update.message.from_user.id
-    im_id = context.user_data['images']
-    image = update.message.document.get_file()
-    image.download(f'cache/{uid}-{im_id}.jpg')
-    context.user_data['images'] += 1
-    update.message.reply_text(f'image {im_id+1} - ok')
-    return CONTENT
-
-def addphoto(update, context):
-    """input: tg photo"""
-    uid = update.message.from_user.id
-    im_id = context.user_data['images']
-    image = update.message.photo[-1].get_file()
-    image.download(f'cache/{uid}-{im_id}.jpg')
-    context.user_data['images'] += 1
-    update.message.reply_text(f'image {im_id+1} - ok')
-    return CONTENT
-    
 def compile_pdf(update, context):
     update.message.reply_text('compiling...\n' 'it can take up to several minutes')
     quality = context.user_data['quality'] if 'quality' in context.user_data else DEFAULT_QUALITY
@@ -101,33 +45,115 @@ def compile_pdf(update, context):
         t = time.time()
         result = subprocess.run(args, shell=True, capture_output=True, check=True, timeout=40)
         t = round(time.time() - t, 2)
-        logger.info(f'compilation success in {t}s.')
+        logger.info(f'compilation success in {t}s. uploading...')
         if result.returncode==0:
-            update.message.reply_text('here is your pdf')
             update.message.reply_document(
                 document=open(pdfname, 'rb'),
                 filename=context.user_data['filename'] + '.pdf'
             )
             os.remove(pdfname)
+            update.message.reply_text('here is your pdf')
+            logger.info(f'done uploading')
         else:
             update.message.reply_text('unknown error. try again later.')
+            logger.exception(f'returncode != 0')
 
         for i in images:
             os.remove(i)
-    except subprocess.CalledProcessError as err:
-        update.message.reply_text('bot error. try again later.')
-        logger.error("magick error: " + err.cmd + "\n>>>" + err.output + "<<<")
     except subprocess.TimeoutExpired as err:
         update.message.reply_text('pdf compilation took too long. try adding less photos or using compression instead of jpg files.')
-        logger.error("magick too long: " + err.cmd + "\n>>>" + err.output + "<<<")
-    except err:
-        logger.error(err)
+        logger.error("compiler error. too long: " + err.cmd + "\n>>>" + err.output + "<<<")
+    except OSError as err:
+        update.message.reply_text('bot error. try again later.')
+        logger.error("compiler error:\n" + str(err))
 
+# --------------------------
+def newpdf(user, quick=False):
+    """Start new PDF"""
+    ustr = str(user.id)
+    if user.username:
+        ustr += f"(t.me/{user.username})"
+    q = "quick " if quick else ""
+    logger.info(f"New {q}pdf u{ustr}")
+
+def newpdf_handler(update, context):
+    user = update.message.from_user
+    newpdf(user)
+    update.message.reply_text(f"New PDF. Enter document name", parse_mode=ParseMode.HTML)
+    return FILENAME
+
+def invalid_filename(update, context):
+    """Prompt one more time"""
+    update.message.reply_text(
+        "Invalid name. Please, use only <code>a-z, A-Z, 0-9, _, .</code> and space characters. Try again or send /cancel",
+        parse_mode=ParseMode.HTML)
+    return FILENAME
+
+def filename_input(update, context):
+    context.user_data['filename'] = update.message.text.strip().split('\n')[0].rstrip()[:MAX_FILENAME_LEN]
+    
+    if 'quick' in context.user_data:
+        compile_pdf(update, context)
+        context.user_data.clear()
+        return ConversationHandler.END
+
+    context.user_data['images'] = 0
+    update.message.reply_text(
+        f'Got it. Now send content of your pdf - photos or *.jpg files. '
+        'When you are ready to compile pdf, send /compile. Send /cancel to cancel.\n'
+        # '<i>Note: pdf pages will have the same orientation as original images. Therefore you need to rotate them before sending</i>'
+        ,parse_mode=ParseMode.HTML
+    )
+    return CONTENT
+
+def save_img(file, update, context):
+    if not 'images' in context.user_data:
+        # Quick way
+        newpdf(update.message.from_user, True)
+        context.user_data['images'] = 0
+        context.user_data['quick'] = True
+        update.message.reply_text('New PDF. Send /compile to finish when you are ready. Send /cancel to cancel.')
+    elif context.user_data['images'] >= MAX_IMG_N:
+        update.message.reply_text("Sorry, maximum image number reached.\n/compile or /cancel")
+        return CONTENT
+    im_id = context.user_data['images']
+    uid = update.message.from_user.id
+    file.download(f'cache/{uid}-{im_id}.jpg')
+    update.message.reply_text(f'image {im_id+1} - ok')
+    context.user_data['images'] = im_id+1
+    return CONTENT
+
+def addfile(update, context):
+    """input: .jpg file"""
+    image = update.message.document.get_file()
+    save_img(image, update, context)
+    return CONTENT
+
+def addphoto(update, context):
+    """input: tg photo"""
+    image = update.message.photo[-1].get_file()
+    save_img(image, update, context)
+    return CONTENT
+
+def compile_handler(update, context):
+    if 'quick' in context.user_data:
+        update.message.reply_text("Enter document name")
+        return FILENAME
+    compile_pdf(update, context)
     context.user_data.clear()
     return ConversationHandler.END
 
 def cancel(update, context):
     update.message.reply_text("Okay, aborting.")
+    if 'images' in context.user_data:
+        uid = update.message.from_user.id
+        im_n = context.user_data['images']
+        images = [f'cache/{uid}-{i}.jpg' for i in range(im_n)]
+        try:
+            for i in images:
+                os.remove(i)
+        except OSError as err:
+            logger.error(f'Cancelling u{uid}. Something gone wrong while deleting cache.\n'+str(err))
     context.user_data.clear()
     return ConversationHandler.END
 
@@ -147,16 +173,33 @@ def quality(update, context):
     return ConversationHandler.END
 # -------------------------
 
+def help_handler(update, context):
+    update.message.reply_text(
+        'I can create PDF from your images: photos (sent with telegram compression, \'as photo\') and *.jpg files (sent without compression, \'as file\').\n'
+        'There are 2 ways:\n'
+        '1. send /start or /newpdf, enter pdf name then send images;\n'
+        '2. just send me some images, then /compile and enter pdf name.\n'
+        'You can cancel operation anytime using /cancel; then you will be able to start again.\n'
+        '<i>By the way, I delete all your images right after sending you PDF.</i>\n\n'
+        'also pls consider donating (servers aren\'t free) - 4441114448609220',
+        parse_mode=ParseMode.HTML
+    )
+
+# -------------------------
+
 def main():
     if not os.path.exists("cache"):
         os.mkdir("cache")
 
     token = None
-    with open('token') as f:
-        token = f.read()
-    if (token is None):
-        logger.fatal("API Token file not found. Aborting.")
-        return
+    try:
+        with open('token') as f:
+            token = f.read()
+    except IOError as ioerr:
+        logger.fatal("Can't access API token file")
+        logger.fatal(ioerr)
+    except:
+        logger.fatal("Can't get API token. Aborting.")
 
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
@@ -168,16 +211,21 @@ def main():
     dp = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler(['start','newpdf'], newpdf)],
+        entry_points=[
+            CommandHandler(['start','newpdf'], newpdf_handler), # Classic way
+            MessageHandler(Filters.document.jpg, addfile), # Quick way
+            MessageHandler(Filters.photo, addphoto)
+        ],
         states={
             FILENAME: [
-                MessageHandler(Filters.regex(r'^[a-zA-Z0-9_][a-zA-Z0-9_.]*$'), filename_input),
-                MessageHandler(~Filters.command, invalid_filename)
+                # MessageHandler(Filters.regex(r'^[a-zA-Z0-9_][a-zA-Z0-9_.]*$'), filename_input),
+                MessageHandler(Filters.text & ~Filters.command, filename_input),
+                # MessageHandler(~Filters.command, invalid_filename)
             ],
             CONTENT: [
                 MessageHandler(Filters.document.jpg, addfile),
                 MessageHandler(Filters.photo, addphoto),
-                CommandHandler('compile', compile_pdf)
+                CommandHandler('compile', compile_handler)
             ],
         },
         fallbacks=[
@@ -185,7 +233,8 @@ def main():
         ]
     )
 
-    dp.add_handler(CommandHandler('quality', quality))
+    # dp.add_handler(CommandHandler('quality', quality))
+    dp.add_handler(CommandHandler('help', help_handler))
     dp.add_handler(conv_handler)
 
     # Start the Bot
